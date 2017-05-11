afaa40b435f11f4043c246f7a95d31654016c74c
Nenna Okoye <ninaokoye@outlook.com>
2017-05-11 18:46:45 +0100

    ready for first deploy

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn ninabuilds.wsgi:application
--- ninabuilds/settings.py
+++ ninabuilds/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'ninabuilds.herokuapp.com']
 
 
 # Application definition
