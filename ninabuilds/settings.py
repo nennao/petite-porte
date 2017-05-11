b7195b921d37872b3f61a5a4cabea2fb084630be
Nenna Okoye <ninaokoye@outlook.com>
2017-05-11 19:04:49 +0100

    modify key var

--- ninabuilds/settings.py
+++ ninabuilds/settings.py
@@ -20,6 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
+SECRET_KEY = my_secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
